a4277acddcc554d383df85ef4d7f40ea1e20f35e
iawais-dev <hello.awais.tahir@gmail.com>
2024-11-01 21:16:33 +0500

    build issue solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Featured from "@/Components/Featured";
 import Footer from "@/Components/Footer";
 import Hero from "@/Components/Hero";
 import Navbar from "@/Components/Navbar";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
